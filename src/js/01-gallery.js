3f19e8aad2a532e9006ef3700669e9ec6437ec32
IrynaRybka <irarybka@gmail.com>
2022-10-21 11:22:26 +0300

    well done gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,45 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 console.log(galleryItems);
+const containerGallery = document.querySelector(".gallery");
+const cardsMarkup = createGalleryMarkup(galleryItems);
+
+containerGallery.insertAdjacentHTML("beforeend", cardsMarkup);
+containerGallery.addEventListener("click", onClickSelectImg);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems.map(({preview, original, description}) => {
+    return `<a class="gallery__item" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}"/>
+         </a>`;
+  }).join('');
+}
+
+function onClickSelectImg(event) {
+  if (event.target.nodeName !== "IMG") {
+      return;
+    }
+    event.preventDefault();
+} 
+
+const gallery = new SimpleLightbox('.gallery__item', {
+    overlayOpacity: 0.5,
+    captionsData: 'alt',
+    captionDelay: 250,
+}); 
+console.log(gallery);
+ 
+
+
+// SimpleLightbox('.gallery__item', {
+//   overlay: true,
+//   overlayOpacity: 0.5,
+//   captionsData: "alt",
+//   captionDelay: 250,
+
+// });
+
