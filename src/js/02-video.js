8c5cc72e7bf245abf0ba6078464878a2b800fa95
IrynaRybka <irarybka@gmail.com>
2022-10-22 13:34:05 +0300

    player is ready

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,16 @@
 import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
-    const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+player.on('timeupdate', throttle(timeToPlay, 1000));
+   
+function timeToPlay({seconds}) {
+    localStorage.setItem('videoplayer-current-time', seconds)
+};
+player.setVolume(0.1); //настройка лично от себя, надоело что громко воспроизводится))
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+
