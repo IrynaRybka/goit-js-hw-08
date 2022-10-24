d742350e30802e4e3e014d549c4e450916f374d4
IrynaRybka <irarybka@gmail.com>
2022-10-24 17:24:21 +0300

    issure

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,20 @@
+const throttle = require('lodash.throttle');
+// import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form"');
+const submit = document.querySelector('.submit');
+
+form.addEventListener('input', throttle(onForm, 500));
+submit.addEventListener('submit', onSubmitBtn)
+
+const formData = {};
+
+function onForm(event){
+    formData = event.target.value;
+    console.log(formData);
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function onSubmitBtn(event){
+console.log(event.target.value)
+}
